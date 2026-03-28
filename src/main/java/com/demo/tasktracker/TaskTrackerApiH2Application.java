812bc2b78fa4fb3bf4c78bc10d10a8083b6dba91
Leopoldo Ramirez Montes <leopoldo@Leopoldos-MacBook-Air.local>
2026-03-27 22:27:07 -0600

    Initial Commit

---
+++ src/main/java/com/demo/tasktracker/TaskTrackerApiH2Application.java
@@ -0,0 +1,13 @@
+package com.demo.tasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApiH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerApiH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/tasktracker/TaskTrackerApiH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApiH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
